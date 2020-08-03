3894f1f2dab942d4106df1d5ffdf2f58ad2e7a4d
Gostas <konst.pap@hotmail.com>
2020-08-03 12:58:46 +0300

    Add Azure Maps primary key string

--- configure.h
+++ configure.h
@@ -1,4 +1,5 @@
 static const char iotConnStr[] = "<replace with IoT Hub connection string>";
+static const String weatherPrimaryKey = "<replace with Azure Maps primary key>";
 
 // Wi-Fi information
 static const char wifi_ssid[] = "<replace with Wi-Fi SSID>";
