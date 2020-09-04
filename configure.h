6500f6bd9e01df1f5a6f7e2ec7260d7fde2780bf
Gostas <konst.pap@hotmail.com>
2020-09-04 19:32:21 +0300

    Add coordinate variables

--- configure.h
+++ configure.h
@@ -4,3 +4,7 @@ static const String weatherPrimaryKey = "<replace with Azure Maps primary key>";
 // Wi-Fi information
 static const char wifi_ssid[] = "<replace with Wi-Fi SSID>";
 static const char wifi_password[] = "<replace with Wi-Fi password>";
+
+//house coordinates
+const float latitude = 0.0;     //replace with your latitude
+const float longitude = 0.0;    //replace with your longitude
